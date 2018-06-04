7cbeb391c016e461052a54a928ce6b859afb0c50
jiro.kirameki <ec2-user@ip-172-31-37-215.us-east-2.compute.internal>
2018-06-04 03:29:33 +0000

    validation

--- resources/views/tasks/edit.blade.php
+++ resources/views/tasks/edit.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 
-    <h1>id: {{ $message->id }} の課題編集ページ</h1>
+    <h1>id: {{ $task->id }} の課題編集ページ</h1>
 
     {!! Form::model($task, ['route' => ['tasks.update', $task->id], 'method' => 'put']) !!}
 
